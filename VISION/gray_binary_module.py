435b152f1d7bc4cb6b5ba3017d149162478b73eb
이희승 <heseung0895@naver.com>
2026-09-06 02:52:32 +0900

    return 컬러를 dst로 변경 / 주석 설명 추

--- VISION/gray_binary_module.py
+++ VISION/gray_binary_module.py
@@ -2,7 +2,7 @@ import cv2
 
 _trackbar_created = False
 
-def empty_callback(val):
+def empty_callback(val):    # OpenCV 함수는 구조상 콜백 함수를 무조건 하나 내놓으라고 요구하기 때문(python OpenCV에선 그럼)
     pass
 
 def apply_binary(frame, window_name="Control"):
@@ -13,12 +13,13 @@ def apply_binary(frame, window_name="Control"):
         cv2.createTrackbar("Binary Threshold", window_name, 128, 255, empty_callback)
         _trackbar_created = True
 
-    # 2. 트랙바 값 읽어오기
+    # 2. 흑백 변환
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # 3. 트랙바 값 읽어오기           현재 트랙바 값보다 크면 255(흰) 같거나 작으면 0(검)
     current_threshold = cv2.getTrackbarPos("Binary Threshold", window_name)
     
-    # 3. 흑백 변환 및 이진화 적용
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # 4. 이진화 적용 (현재 값보다 크면 흰색, 작으면 검은색)
     _, dst = cv2.threshold(gray_frame, current_threshold, 255, cv2.THRESH_BINARY)
     
-    # FaceID.py의 화면 출력 형식을 맞추기 위해 다시 3채널(BGR)로 변환
-    return cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    return dst
